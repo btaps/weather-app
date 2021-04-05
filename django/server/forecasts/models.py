03e2cb98fe2907978b27cf24a9b0bc0de0bbe89c
Bruno Tapia Sierra <btapiasierra@outlook.com>
2021-04-05 12:54:42 -0700

    Added simple tests for backend

--- django/server/forecasts/models.py
+++ django/server/forecasts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Forecasts(models.Model):
     city = models.CharField(max_length=20)
     country = models.CharField(max_length=20)
--- django/server/forecasts/tests.py
+++ django/server/forecasts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Forecasts
 
-# Create your tests here.
+
+class BasicTest(TestCase):
+    def setUp(self):
+        Forecasts.objects.create(
+            city="Reno", country="US", weather="Cloudy", temp=65)
+        pass
+
+    def test_forecast_saved_correctly(self):
+        forecast = Forecasts.objects.get(pk=1)
+        self.assertEqual(forecast.city, "Reno")
+        self.assertEqual(forecast.country, "US")
+        self.assertEqual(forecast.weather, "Cloudy")
+        self.assertEqual(forecast.temp, 65)
+
+    def test_city_max_length(self):
+        forecast = Forecasts.objects.get(pk=1)
+        max_length = forecast._meta.get_field("city").max_length
+        self.assertEqual(max_length, 20)
+
+    def test_get_forecast(self):
+        response = self.client.get("/api/forecasts/1/")
+        self.assertEqual(response.status_code, 200)
